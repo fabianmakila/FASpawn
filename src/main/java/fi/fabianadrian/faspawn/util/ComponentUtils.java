fcd82396984295e8dc29394350be45705403c9b5
FabianAdrian <contact@fabianadrian.fi>
2024-08-25 22:59:56 +0300

    final

--- src/main/java/fi/fabianadrian/faspawn/util/ComponentUtils.java
+++ src/main/java/fi/fabianadrian/faspawn/util/ComponentUtils.java
@@ -3,8 +3,7 @@ package fi.fabianadrian.faspawn.util;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 
-public class ComponentUtils {
-
+public final class ComponentUtils {
 	public static Component locationComponent(Location location) {
 		String content = String.format(
 				"%.1f, %.1f, %.1f [%.1f, %.1f]",
